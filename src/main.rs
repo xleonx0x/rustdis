e2a507cc4f4fd4dfe32ea0631cdfc6223ca17800
chris <123799877+xleonx0x@users.noreply.github.com>
2026-05-01 21:31:58 +1000

    feat: added basic tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    
+    let bytes_read = stream.read(&mut buf);
+
+    stream.write(b"PING\n");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = TcpListener::bind("127.0.0.1:6767").unwrap();
+
+    for stream in conn.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("new connection received");
+                thread::spawn(|| {
+                    handle_connection(stream);
+                });
+            }
+            Err(_) => todo!(),
+        }
+    }
 }
